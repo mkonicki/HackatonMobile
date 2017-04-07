bc89abfd5f84597d079a2bbf771be2ba8c378c9e
Limak <kkoniki@gazeta.pl>
2017-04-07 23:26:45 +0200

    Aktualizacja widokow Places master/details

--- app/src/main/java/konicki/mateusz/hackatonapp/places/PlaceDetailFragment.java
+++ app/src/main/java/konicki/mateusz/hackatonapp/places/PlaceDetailFragment.java
@@ -10,11 +10,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import konicki.mateusz.hackatonapp.R;
-import konicki.mateusz.hackatonapp.places.dummy.DummyContent;
+import konicki.mateusz.hackatonapp.model.Place;
 
 /**
  * A fragment representing a single Place detail screen.
- * This fragment is either contained in a {@link PlaceListActivity}
  * in two-pane mode (on tablets) or a {@link PlaceDetailActivity}
  * on handsets.
  */
@@ -28,7 +27,7 @@ public class PlaceDetailFragment extends Fragment {
     /**
      * The dummy content this fragment is presenting.
      */
-    private DummyContent.DummyItem mItem;
+    private Place place;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -45,12 +44,12 @@ public class PlaceDetailFragment extends Fragment {
             // Load the dummy content specified by the fragment
             // arguments. In a real-world scenario, use a Loader
             // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+            //place = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
             CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
             if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
+                //appBarLayout.setTitle(place.content);
             }
         }
     }
@@ -61,8 +60,8 @@ public class PlaceDetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.place_detail, container, false);
 
         // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.place_detail)).setText(mItem.details);
+        if (place != null) {
+            //((TextView) rootView.findViewById(R.id.place_detail)).setText(mItem.details);
         }
 
         return rootView;
