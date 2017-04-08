5d48fd5db3e57c2cb3dedfc7a63734394ec4002e
Limak <kkoniki@gazeta.pl>
2017-04-08 02:48:40 +0200

    Nowy adapter dla Summary activity Fix w Summary

--- app/src/main/java/konicki/mateusz/hackatonapp/summary/Summary.java
+++ app/src/main/java/konicki/mateusz/hackatonapp/summary/Summary.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import konicki.mateusz.hackatonapp.R;
+import konicki.mateusz.hackatonapp.database.dbhelper.DBHelper;
 
 public class Summary extends AppCompatActivity {
 
@@ -22,38 +23,8 @@ public class Summary extends AppCompatActivity {
         setContentView(R.layout.activity_summary);
 
         final ListView listview = (ListView) findViewById(R.id.summary_view);
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile" };
 
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
-        final StableArrayAdapter adapter = new StableArrayAdapter(this, R.layout.summary_list_item, list);
+        final SummaryListAdapter adapter = new SummaryListAdapter(this, new DBHelper(this).getSession().getBeaconDao().loadAll());
         listview.setAdapter(adapter);
-    }
-
-    private class StableArrayAdapter extends ArrayAdapter<String> {
-
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
     }
 }
